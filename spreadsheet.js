4dda894dd4a9d0f79a5d67b03218ee5b649c34d8
nicoleal88 <nicoleal88@gmail.com>
2020-07-06 10:44:13 -0300

    access to spreadsheet

--- spreadsheet.js
+++ spreadsheet.js
@@ -1,1 +1,16 @@
 const GoogleSpreadsheet = require('google-spreadsheet');
+
+const { promisify } = require('util');
+
+const creds = require('client_secret.json');
+
+async function accessSpreadsheet(){
+    const doc = new GoogleSpreadsheet('1CBv7tgO901AIRwU08c702O4S-f-kP3eXiQwEjb-VqXU');
+    await promisify(doc.useServiceAccountAuth)(creds);
+    const info = await promisify(doc.getInfo)();
+    const sheet = info.worksheets[5];
+    console.log(`Title: ${sheet.title}, Rows: ${sheet.rowCount}`);
+
+}
+
+accessSpreadsheet();
