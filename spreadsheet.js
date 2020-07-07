7103ef14e3a9da2dbc6d4fa6b47b5a3458ac8155
Ubuntu <ubuntu@ip-172-31-37-12.us-east-2.compute.internal>
2020-07-07 00:07:54 +0000

    Some changes

--- spreadsheet.js
+++ spreadsheet.js
@@ -1,16 +1,22 @@
-const GoogleSpreadsheet = require('google-spreadsheet');
+const { GoogleSpreadsheet } = require('google-spreadsheet');
 
-const { promisify } = require('util');
+// spreadsheet key is the long id in the sheets URL
+//const doc = new GoogleSpreadsheet('1CBv7tgO901AIRwU08c702O4S-f-kP3eXiQwEjb-VqXU');
 
-const creds = require('client_secret.json');
+//await doc.useServiceAccountAuth(require('./client_secret.json'));
+
+//await doc.loadInfo(); // loads document properties and worksheets
+
+//console.log(doc.title);
 
 async function accessSpreadsheet(){
-    const doc = new GoogleSpreadsheet('1CBv7tgO901AIRwU08c702O4S-f-kP3eXiQwEjb-VqXU');
-    await promisify(doc.useServiceAccountAuth)(creds);
-    const info = await promisify(doc.getInfo)();
-    const sheet = info.worksheets[5];
-    console.log(`Title: ${sheet.title}, Rows: ${sheet.rowCount}`);
-
+	    const doc = new GoogleSpreadsheet('1CBv7tgO901AIRwU08c702O4S-f-kP3eXiQwEjb-VqXU');
+	    await doc.useServiceAccountAuth(require('./client_secret.json'));
+	    await doc.loadInfo();
+	    console.log(doc.title);
+	    const otherSheet = doc.sheetsByIndex[5];
+	console.log(otherSheet);
 }
 
 accessSpreadsheet();
+
